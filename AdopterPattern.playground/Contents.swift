94740e580c6f27b52c1038e3e1921d363f04e0c0
Rin <rin.t.4921@gmail.com>
2022-10-25 21:13:53 +0900

    AdopterPatternの解説を追加

--- AdopterPattern.playground/Contents.swift
+++ AdopterPattern.playground/Contents.swift
@@ -1,3 +1,66 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+protocol DataStoreProtocol {
+    func add()
+    func delete()
+}
+
+
+class OwnServerDataStore: DataStoreProtocol {
+
+    func add() {
+        print("サーバーにデータを保存しました")
+    }
+
+    func delete() {
+        print("サーバーからデータを削除しました")
+    }
+}
+
+class FirebaseDataStore {
+
+    func addToFirebase() {
+        print("Firebaseにデータを保存しました")
+    }
+
+    func deleteFromFirebase() {
+        print("Firebaseからデータを削除しました")
+    }
+}
+
+class FirebaseDataStoreAdopter: DataStoreProtocol {
+
+    private let firebaseDataStore = FirebaseDataStore()
+
+    func add() {
+        firebaseDataStore.addToFirebase()
+    }
+
+    func delete() {
+        firebaseDataStore.deleteFromFirebase()
+    }
+}
+
+
+class Client {
+
+    private let dataStore: DataStoreProtocol
+
+    init(dataStore: DataStoreProtocol) {
+        self.dataStore = dataStore
+    }
+
+    func addData() {
+        dataStore.add()
+    }
+
+    func deleteData() {
+        dataStore.delete()
+    }
+}
+
+let client = Client(dataStore: FirebaseDataStoreAdopter())
+client.addData()
+client.deleteData()
+
